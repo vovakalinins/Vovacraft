3dadf406f1b435037e2062e35e443794948d90ad
vovakalinins <vovakalinin24@icloud.com>
2026-01-19 11:43:39 -0500

    blank window opengl

---
+++ readme.md
@@ -0,0 +1,2 @@
+This is a very bad minecaft clone, it is made in c++ 17 and opengl.
+you do need to compile it yourself.
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vovacraft", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create glfw window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0,0,800,600);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
